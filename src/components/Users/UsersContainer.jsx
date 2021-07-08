66c0e780478544461299a332a4219cfa50f4dee0
metter <bonnydse@mail.ru>
2021-07-09 02:05:12 +0700

    separated pure component from container component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import Users from './Users';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersTotalCountAC } from './../../redux/users-reducer';
-import usersC from './UsersClass';
+import * as axios from 'axios'
+import Users from './Users';
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=
+        ${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            console.log(response);
+            this.props.setUsers(response.data.items)
+            this.props.setUsersTotalCount(response.data.totalCount)
+
+        });
+    }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=
+        ${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <Users onPageChanged={this.onPageChanged} 
+            currentPage={this.props.currentPage}
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            users={this.props.users}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+
+        />
+    }
+}
+
 
 let mapStateToProps = (state) => {
     return {
@@ -34,7 +67,7 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(usersC);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 
 export default UsersContainer;
